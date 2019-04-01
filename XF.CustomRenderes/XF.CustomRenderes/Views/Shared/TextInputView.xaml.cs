d2d231495e4cc9c1d1a91c55568fc9f9c7a95383
André Barbosa <dev.andrebarbosa@outlook.com>
2019-04-01 16:41:19 -0300

    Disponibilização do componente de input como contentview

---
+++ XF.CustomRenderes/XF.CustomRenderes/Views/Shared/TextInputView.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Xamarin.Forms;
+
+namespace XF.CustomRenderes.Views.Shared
+{
+    public partial class TextInputView : ContentView
+    {
+        public TextInputView()
+        {
+            InitializeComponent();
+        }
+
+        #region Properties
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly BindableProperty IconProperty =
+            BindableProperty.Create(nameof(Icon), typeof(string), typeof(TextInputView), string.Empty, propertyChanged: OnIconChanged);
+
+        private static void OnIconChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = (TextInputView)bindable;
+
+            view.imgIcon.Source = (string)newValue;
+        }
+
+        public string PlaceholderText
+        {
+            get => (string)GetValue(PlaceholderTextProperty);
+            set => SetValue(PlaceholderTextProperty, value);
+        }
+
+        public static readonly BindableProperty PlaceholderTextProperty =
+            BindableProperty.Create(nameof(PlaceholderText), typeof(string), typeof(TextInputView), string.Empty, propertyChanged: OnPlaceholderTextChanged);
+
+        private static void OnPlaceholderTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = (TextInputView)bindable;
+
+            view.entryText.Placeholder = (string)newValue;
+        }
+
+        #endregion
+    }
+}
